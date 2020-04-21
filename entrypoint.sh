7774fe8b070b4ee71fa10fd1d0c8d720045150bb
zhujq <38308874+zhujq@users.noreply.github.com>
2020-04-21 09:16:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-cd /v2ray
+/* cd /v2ray
 wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip v2ray.zip 
 if [ ! -f "v2ray" ]; then
@@ -10,5 +10,6 @@ fi
 
 cp -f /config.json .
 chmod +x v2ray v2ctl
+*/
 nohup /usr/sbin/sshd -D 
-./v2ray
+// ./v2ray
