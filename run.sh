6323eeb8c9fd67a70b294d590019ba6db6b67a7b
zhujq <38308874+zhujq@users.noreply.github.com>
2021-02-14 16:05:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir -p /var/run/sshd
 nohup /usr/sbin/sshd -D &
 
 cd /v2ray
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+wget -O v2ray.zip http://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
 unzip v2ray.zip 
 if [ ! -f "v2ray" ]; then
   mv /v2ray/v2ray-v$VER-linux-64/v2ray .
